78dd4cbf5b79b539b9d9fa1b72788a901a268b18
Waqas <essamamdani2003@Pc-2.local>
2015-07-22 12:18:13 +0500

    First Application

---
+++ app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Horsefly\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Horsefly\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class Request extends FormRequest
+{
+    //
+}
